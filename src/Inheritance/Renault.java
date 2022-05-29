5b9498f8a0be14da7de106ea74a3997b8d73ed02
Pierre Rambert <pierrerambert@Pierres-MacBook-Pro-2.local>
2022-05-29 22:58:38 +0200

    Inheritance done ; 4 to go

---
+++ src/Inheritance/Renault.java
@@ -0,0 +1,26 @@
+package Inheritance;
+
+public class Renault extends Voiture {
+
+//    @Override
+    public void tutut() {
+        System.out.println("Renault: Tut tuuut !");
+    }
+
+    public static void main(String[] args) {
+        Renault clio = new Renault();
+        clio.honk();
+//        Ici, clio ne voit pas honk() dans la classe Renault
+//        Mais comme Renault est la classe fille de Voiture et que honk() est definie dans Voiture
+//        Alors clio peut appeler honk() de Voiture.
+
+        clio.tutut();
+//        Ici, c'est bien le tutut de Renalt qui est utilise
+//        Puisque la methode est override en runtime
+
+        Voiture voiture = new Renault();
+        voiture.tutut();
+//        Meme chose ici
+
+    }
+}
---
+++ src/Inheritance/Voiture.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class Voiture {
+
+    public void honk() {
+        System.out.println("Honk hooonk !");
+    }
+
+    public void tutut() {
+        System.out.println("Voiture: Tut tuuut !");
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
